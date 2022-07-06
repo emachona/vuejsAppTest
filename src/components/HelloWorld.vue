7527712fdd084271b077c8c34f94a988ee2dfda7
emachona <emmachona01@gmail.com>
2022-07-06 12:37:05 +0200

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,43 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
+  <div class="flex">
+        <h3>GET METHOD</h3>
+        <button 
+          class="btn"
+          @click="getData()"
+        >
+          get data from internet
+        </button>
+        <h4 v-show="value">
+        {{showData}}
+      </h4>
+      </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      value:true,
+      showData:""
+    };
+  },
+  methods:{
+    async getData(){
+      this.value=true;
+      this.response=await axios.get(' https://api.roastandbrew.coffee/api/v1/companies')
+      if(this.response.status===200){
+        this.showData=this.response.data;
+      }
+    }
   }
 }
 </script>
---
+++ src/views/GalleryView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="gallery">
+    <h1>This is the gallery page</h1>
+  </div>
+</template>
+
+<script>
+import GalleryComponent from '@/components/GalleryComponent.vue'
+
+export default {
+         name: 'GalleryView',
+        Components: {
+           GalleryComponent
+        }
+}
+</script>
